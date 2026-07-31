56f6d1814487359946adb01682f1dcbef960e8d5
jonathangana131-lab <jonathangana131@gmail.com>
2026-07-30 22:21:18 -0700

    Connect live ride feedback to the app

--- Voltline/App/Sources/VoltlineGameApp.swift
+++ Voltline/App/Sources/VoltlineGameApp.swift
@@ -7,6 +7,7 @@ struct VoltlineGameApp: App {
     var body: some Scene {
         WindowGroup {
             GameRootView(session: session)
+                .background(GameFeedbackBridge(session: session))
                 .preferredColorScheme(.dark)
                 .persistentSystemOverlays(.hidden)
         }
