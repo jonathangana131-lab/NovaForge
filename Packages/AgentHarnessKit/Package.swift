c506e2d1b18348b2dea4c2fcc5c3569c75c25de1
jonathangana131-lab <jonathangana131@gmail.com>
2026-08-07 05:44:39 -0700

    Expose ForgeMission package target

--- Packages/AgentHarnessKit/Package.swift
+++ Packages/AgentHarnessKit/Package.swift
@@ -17,6 +17,7 @@ let package = Package(
         .library(name: "AgentShadow", targets: ["AgentShadow"]),
         .library(name: "AgentPolicy", targets: ["AgentPolicy"]),
         .library(name: "AgentTransport", targets: ["AgentTransport"]),
+        .library(name: "ForgeMission", targets: ["ForgeMission"]),
     ],
     targets: [
         .target(name: "AgentDomain"),
@@ -64,6 +65,7 @@ let package = Package(
             name: "AgentTransport",
             dependencies: ["AgentDomain"]
         ),
+        .target(name: "ForgeMission"),
         .testTarget(
             name: "AgentDomainTests",
             dependencies: ["AgentDomain"]
@@ -109,6 +111,10 @@ let package = Package(
         .testTarget(
             name: "AgentTransportTests",
             dependencies: ["AgentDomain", "AgentTransport"]
+        ),
+        .testTarget(
+            name: "ForgeMissionTests",
+            dependencies: ["ForgeMission"]
         ),
     ]
 )
