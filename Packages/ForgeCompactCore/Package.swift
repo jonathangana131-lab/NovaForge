5263ec72e52bdedda31178316d74fa62b509e128
jonathangana131-lab <jonathangana131@gmail.com>
2026-08-08 04:22:45 -0700

    Add Forge Compact package manifest

---
+++ Packages/ForgeCompactCore/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "ForgeCompactCore",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+    ],
+    products: [
+        .library(name: "ForgeCompactCore", targets: ["ForgeCompactCore"]),
+    ],
+    targets: [
+        .target(name: "ForgeCompactCore"),
+        .testTarget(name: "ForgeCompactCoreTests", dependencies: ["ForgeCompactCore"]),
+    ]
+)
