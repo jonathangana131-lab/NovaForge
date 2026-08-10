1297089b78a65d8b8b149295b0ee0977d63dc983
jonathangana131-lab <jonathangana131@gmail.com>
2026-08-10 00:55:34 -0700

    [V14] Add Forge playtest package shell

---
+++ Packages/ForgePlaytestCore/Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let package = Package(
+    name: "ForgePlaytestCore",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+    ],
+    products: [
+        .library(name: "ForgePlaytestCore", targets: ["ForgePlaytestCore"]),
+    ],
+    targets: [
+        .target(name: "ForgePlaytestCore"),
+        .testTarget(name: "ForgePlaytestCoreTests", dependencies: ["ForgePlaytestCore"]),
+    ]
+)
